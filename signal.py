ec9d06faea8b33847b39d235651d528a45a0889e
Evgeniy Galiullin <zhenya-skyline@mail.ru>
2023-01-23 21:54:29 +0500

    испраление ошибок
    подключен модуль lirc

--- signal.py
+++ signal.py
@@ -14,71 +14,71 @@ class DHT11Result:
     temperature = -1
     humidity = -1
 
-    def __init__(self, error_code, temperature, humidity):
-        self.error_code = error_code
-        self.temperature = DHT11.T
-        self.humidity = DHT11.H
+    def __init__(client, error_code, temperature, humidity):
+        client.error_code = error_code
+        client.temperature = temperature
+        client.humidity = humidity
 
-    def is_valid(self):
-        return self.error_code == DHT11Result.ERR_NO_ERROR
+    def is_valid(client):
+        return client.error_code == DHT11Result.ERR_NO_ERROR
 
 #Считывателя датчиков DHT11
 class DHT11:
 
     __pin = 0
 
-    def __init__(self, pin):
-        self.__pin = pin
+    def __init__(client, pin):
 
-    def read(self):
-        lirc.GPIO.setup(self.__pin, lirc.GPIO.OUT)
+        client = lirc.Client()
+        client.__pin = pin
+
+    def read(client):
+        
+        # client.send_once("my-remote-name", "KEY_3", repeat_count=1)
+        # client.send_once("my-remote-name", "KEY_POWER")
+        
+        lirc.GPIO.setup(client.__pin, lirc.GPIO.OUT)
 
         # отправить начальный максимум
-        self.__send_and_sleep(lirc.GPIO.HIGH, 0.05)
+        client.send_once(lirc.GPIO.HIGH, 0.05)
 
         # уменьшите до минимума
-        self.__send_and_sleep(lirc.GPIO.LOW, 0.02)
+        client.send_once(lirc.GPIO.LOW, 0.02)
 
         # изменить ввод с помощью подтягивания
-        lirc.GPIO.setup(self.__pin, lirc.GPIO.IN, lirc.GPIO.PUD_UP)
+        lirc.GPIO.setup(client.__pin, lirc.GPIO.IN, lirc.GPIO.PUD_UP)
 
         # собирать данные в массив
-        data = self.__collect_input__collect_input()
+        data = client.__collect_input()
 
         # анализировать длины всех периодов извлечения данных
-        pull_up_lengths = self.__parse_data_pull_up_lengths__parse_data_pull_up_lengths(data)
+        pull_up_lengths = client.__parse_data_pull_up_lengths__parse_data_pull_up_lengths(data)
 
         # если количество битов не совпадает, возвращается ошибка (4 байта данных + 1 байт контрольной суммы)
         if len(pull_up_lengths) != 40:
             return (DHT11Result.ERR_MISSING_DATA, 0, 0)
 
         # вычислять биты из длин периодов подтягивания
-        bits = self.__calculate_bits(pull_up_lengths)
+        bits = client.__calculate_bits(pull_up_lengths)
 
         # у нас есть биты, вычисляем байты
-        the_bytes = self.__bits_to_bytes(bits)
+        the_bytes = client.__bits_to_bytes(bits)
 
         # вычислить контрольную сумму и проверить
-        checksum = self.__calculate_checksum(the_bytes)
+        checksum = client.__calculate_checksum(the_bytes)
         if the_bytes[4] != checksum:
             return DHT11Result(DHT11Result.ERR_CRC, 0, 0)
 
-        # Значение возвращаемых значений датчика
-        # the_bytes[0]: значение влажности
-        # the_bytes[1]: десятичная влажность
-        # the_bytes[2]: значение температуры
-        # the_bytes[3]: десятичная температура
-
-        temperature = the_bytes[2] + float(the_bytes[3]) / 10
-        humidity = the_bytes[0] + float(the_bytes[1]) / 10
+        temperature = the_bytes[2] + float(the_bytes[3]) / 10 # температура
+        humidity = the_bytes[0] + float(the_bytes[1]) / 10 # влажность
 
         return DHT11Result(DHT11Result.ERR_NO_ERROR, temperature, humidity)
 
-    def __send_and_sleep(self, output, sleep):
-        lirc.GPIO.output(self.__pin, output)
+    def send_once(client, output, sleep):
+        lirc.GPIO.output(client.__pin, output)
         time.sleep(sleep)
 
-    def __collect_input(self):
+    def __collect_input(client):
         # собирать данные, пока они не найдены
         unchanged_count = 0
 
@@ -88,7 +88,7 @@ class DHT11:
         last = -1
         data = []
         while True:
-            current = lirc.GPIO.input(self.__pin)
+            current = lirc.GPIO.input(client.__pin)
             data.append(current)
             if last != current:
                 unchanged_count = 0
@@ -100,7 +100,7 @@ class DHT11:
 
         return data
 
-    def __parse_data_pull_up_lengths(self, data):
+    def __parse_data_pull_up_lengths(client, data):
         STATE_INIT_PULL_DOWN = 1
         STATE_INIT_PULL_UP = 2
         STATE_DATA_FIRST_PULL_DOWN = 3
@@ -162,7 +162,7 @@ class DHT11:
 
         return lengths
 
-    def __calculate_bits(self, pull_up_lengths):
+    def __calculate_bits(client, pull_up_lengths):
 
         # найти самый короткий и самый длинный период
         shortest_pull_up = 1000
@@ -187,7 +187,7 @@ class DHT11:
 
         return bits
 
-    def __bits_to_bytes(self, bits):
+    def __bits_to_bytes(client, bits):
         the_bytes = []
         byte = 0
 
@@ -203,5 +203,5 @@ class DHT11:
 
         return the_bytes
 
-    def __calculate_checksum(self, the_bytes):
+    def __calculate_checksum(client, the_bytes):
         return the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3] & 255
